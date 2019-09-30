f9dd57d97a08325c36f8d5def88871b01faf9a74
Ricardo Montesinos <ricardo.montesinos@globant.com>
2019-09-30 12:07:35 -0500

    POD Configuration, Alamofire, RxSwift, Hero, Lottie and Realm

---
+++ TheMovieApp/TheMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieApp
+//
+//  Created by Richie on 9/30/19.
+//  Copyright © 2019 Rappi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
