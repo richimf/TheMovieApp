e205649439160166a69e187412c93ba056e02973
Ricardo Montesinos <ricardo.montesinos@globant.com>
2019-10-06 20:37:39 -0500

    Refactor Image downloading in Detail View

--- TheMovieApp/TheMovieApp/MovieDetail/Interactor/MovieDetailInteractor.swift
+++ TheMovieApp/TheMovieApp/MovieDetail/Interactor/MovieDetailInteractor.swift
@@ -18,43 +18,22 @@ class MovieDetailInteractor: MovieDetailInteractorInputProtocol {
   private let dataManager = DataManager()
   
   var coverImage: UIImage?
-
+  
   func loadImage(of movie: Movie) {
-    guard let path = movie.backdropPath else { return }
     let key = NSString(string: "\(String(describing: movie.id))backdropImage")
-    let fullPath: String = "\(APIUrls.img.rawValue)\(path)"
-    let cache = cacheDataManager.imageCache
-    
     // GET FROM LOCAL
     if !Connectivity.isConnectedToInternet {
-      let dataManager = DataManager()
-      dataManager.retrieveImageDataFrom(key: key as String) { data in
-        guard let data = data else { return }
-        if let image =  UIImage(data: data) {
-          self.presenter?.loadImage(image)
-        }
-      }
+      getOfflineImageWith(key: key as String)
       return
     }
     // LOAD FROM CACHE
+    let cache = cacheDataManager.imageCache
     if let cachedImage = cache.object(forKey: key) {
       self.coverImage = cachedImage
       presenter?.loadImage(cachedImage)
     } else {
-      // DOWNLOAD
-      DispatchQueue.global(qos: .background).async {
-        guard
-          let url: URL = URL(string: fullPath),
-          let data = try? Data(contentsOf: url),
-          let image: UIImage = UIImage(data: data)
-          else { return }
-        DispatchQueue.main.async {
-          cache.setObject(image, forKey: key)
-          self.saveImageLocallyWith(key: key as String, and: data)
-          self.coverImage = image
-          self.presenter?.loadImage(image)
-        }
-      }
+      //DOWNLOAD
+      downloadImage(of: movie, key: key as String)
     }
   }
   
@@ -64,14 +43,42 @@ class MovieDetailInteractor: MovieDetailInteractorInputProtocol {
     if !Connectivity.isConnectedToInternet {
       dataManager.retrieveImageDataFrom(key: key) { data in
         guard let data = data else { return }
-        image =  UIImage(data: data)
+        image = UIImage(data: data)
       }
     }
     return image
+  }
+  
+  private func getOfflineImageWith(key: String) {
+    let dataManager = DataManager()
+    dataManager.retrieveImageDataFrom(key: key) { data in
+      guard let data = data else { return }
+      if let image = UIImage(data: data) {
+        self.presenter?.loadImage(image)
+      }
+    }
+  }
+  
+  private func downloadImage(of movie: Movie, key: String) {
+    guard let path = movie.backdropPath else { return }
+    let fullPath: String = "\(APIUrls.img.rawValue)\(path)"
+    let cache = cacheDataManager.imageCache
+    DispatchQueue.global(qos: .background).async {
+      guard
+        let url: URL = URL(string: fullPath),
+        let data = try? Data(contentsOf: url),
+        let image: UIImage = UIImage(data: data)
+        else { return }
+      DispatchQueue.main.async {
+        cache.setObject(image, forKey: key as NSString)
+        self.saveImageLocallyWith(key: key, and: data)
+        self.coverImage = image
+        self.presenter?.loadImage(image)
+      }
+    }
   }
 
   private func saveImageLocallyWith(key: String, and data: Data?) {
     dataManager.saveImageDataFor(key: key, and: data)
   }
-
 }
