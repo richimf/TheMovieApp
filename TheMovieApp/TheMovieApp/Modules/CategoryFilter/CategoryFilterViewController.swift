f30191279d3174c9a9e652008538f0fcb626fbbf
Ricardo Montesinos <ricardo.montesinos@globant.com>
2019-10-07 23:55:00 -0500

    Update comments

--- TheMovieApp/TheMovieApp/Modules/CategoryFilter/CategoryFilterViewController.swift
+++ TheMovieApp/TheMovieApp/Modules/CategoryFilter/CategoryFilterViewController.swift
@@ -2,7 +2,7 @@
 //  CategoryFilterViewController.swift
 //  TheMovieApp
 //
-//  Created by Richie on 10/7/19.
+//  Created by Ricardo Montesinos on 10/1/19.
 //  Copyright © 2019 Rappi. All rights reserved.
 //
 
